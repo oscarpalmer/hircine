336dae1152d39b1c9319575f6e6efe13e4ff0243
Oscar Palmér <opalmer@me.com>
2014-06-08 20:00:37 +0200

    0.6.0
    Updated the internals to utilize the good stuff available in modern browsers.

--- src/hircine.js
+++ src/hircine.js
@@ -10,7 +10,6 @@
   var
   win        = this,
   doc        = win.document,
-  htmlString = /^\s*<(\w+|!)[^>]*>/,
   quickFind  = /^(?:\w*\#([\w\-]+)|\.([\w\-]+)|(\w+))$/,
   Hircine;
 
@@ -38,63 +37,12 @@
     if (typeof context === "undefined") {
       return doc;
     } else if (typeof context === "string") {
-      return hircine(context);
+      return Hircine(context);
     } else if (isNodeLike(context)) {
       return context;
     }
 
     return doc;
-  }
-
-  /**
-   * Looper.
-   */
-  function each(obj, fn, scope) {
-    var
-    index = 0,
-    langd = obj.length;
-
-    for (; index < langd; index++) {
-      fn(obj[index], index);
-    }
-
-    return obj;
-  }
-
-  /**
-   * Create real HTML from a string.
-   */
-  function htmlify(string) {
-    var
-    html    = doc.createElement("div"),
-    results = [];
-
-    html.innerHTML = string;
-
-    each(html.childNodes, function(node) {
-      if (isNodeLike(node)) {
-        results.push(node);
-      }
-    });
-
-    return results;
-  }
-
-  /**
-   * Check if item exists in array.
-   */
-  function inArray(obj, item) {
-    var
-    index = 0,
-    langd = obj.length;
-
-    for (; index < langd; index++) {
-      if (obj[index] === item) {
-        return true;
-      }
-    }
-
-    return false;
   }
 
   /**
@@ -116,13 +64,15 @@
    */
   function toArray(obj) {
     var
+    index   = 0,
+    langd   = obj.length,
     results = [];
 
-    each(obj, function(item) {
-      if (inArray(results, item) !== true) {
-        results.push(item);
+    for (; index < langd; index++) {
+      if (results.indexOf(obj[index]) === -1) {
+        results.push(obj[index]);
       }
-    });
+    }
 
     return results;
   }
@@ -137,7 +87,8 @@
   Hircine = function(selector, context) {
     var
     results = [],
-    match;
+    index,
+    langd;
 
     if (typeof selector === "undefined") {
       return results;
@@ -154,9 +105,12 @@
     context = defineContext(context);
 
     if (likeArray(context)) {
-      each(context, function(node) {
-        results = results.concat(hircine(selector, node));
-      });
+      index = 0;
+      langd = context.length;
+
+      for (; index < langd; index++) {
+        results = results.concat(Hircine(selector, context[index]));
+      }
 
       return toArray(results);
     }
@@ -165,9 +119,7 @@
       return results;
     }
 
-    match = quickFind.exec(selector);
-
-    if (match) {
+    if ((match = quickFind.exec(selector))) {
       if (match[1]) {
         return byId(context, match[1]);
       } else if (match[2] && context.getElementsByClassName) {
@@ -175,10 +127,6 @@
       } else if (match[3]) {
         return toArray(context.getElementsByTagName(match[3]));
       }
-    }
-
-    if (htmlString.exec(selector)) {
-      return htmlify(selector);
     }
 
     if (context.querySelectorAll) {
